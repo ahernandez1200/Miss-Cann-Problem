26c4bb7b3558c212430ba7604842b55c0b6fd265
ahernandez1200 <tony.6.h@gmail.com>
2021-09-17 15:48:18 -0700

    fully implement action and result

--- miss_cann_test.py
+++ miss_cann_test.py
@@ -4,7 +4,7 @@ class MissCannibals(Problem):
     def __init__(self, initial, goal=(0, 0, False)):
         super().__init__(initial, goal)
 
-    """val is the either the current amount of miss on left,
+    """val is  either the current amount of miss on left,
      or the current amount of canniabals on the left. Seats refers
      to the # of people to be sent on the boat. This function checks
      to see if sending seats amount of people to the other side 
@@ -26,13 +26,14 @@ class MissCannibals(Problem):
         return False
 
     def actions(self, state):
-        stateList = list(state)
-        miss = stateList[0]
-        cann = stateList[1]
-        on_left = stateList[2]
+        stateList = list(state) #converting tuple to list
+        miss = stateList[0] #the number of missionaries on left side
+        cann = stateList[1] #the number of cannibals on left side
+        on_left = stateList[2] #whether the boat is on the left side
 
-        possible_actions = []
-        pos_or_neg = 1
+        action = "" #where we will temporarily hold our actions 
+        possible_actions = [] #list of all possible actions 
+        pos_or_neg = 1 # 1 if moving to the left, -1 if moving to the right.
 
         """sending pieces to the right is like subtracting, while sending
             to the left is like adding. So if we are moving pieces to the
@@ -44,22 +45,44 @@ class MissCannibals(Problem):
         for x in range(1, 3):
             if self.within_bounds(miss, x*pos_or_neg):
                 if not self.cann_outnumber_miss(miss + x*pos_or_neg, cann):
-                    possible_actions.append([x*pos_or_neg, 0, not on_left])
+                    possible_actions.append(action.ljust(x, "M"))
         
         """Loop checks to see if sending 1 cannibal is a valid move,
             and then checks to see if sending 2 cannibals is a valid move"""
         for x in range(1,3):
             if self.within_bounds(cann, x * pos_or_neg):
-                print(self.cann_outnumber_miss(miss, cann + x*pos_or_neg))
                 if not self.cann_outnumber_miss(miss, cann + x*pos_or_neg):
-                    possible_actions.append([0, x * pos_or_neg, not on_left])
+                    possible_actions.append(action.ljust(x, "C" ))
         
         #checks to see if sending 1 cannibal and 1 missionary is a valid move
         if self.within_bounds(cann, pos_or_neg) and self.within_bounds(miss, pos_or_neg):
             if not self.cann_outnumber_miss(miss + pos_or_neg, cann + pos_or_neg):
-                possible_actions.append([pos_or_neg, pos_or_neg, not on_left])
+                possible_actions.append("MC")
         
         return possible_actions
+
+    #determines a new state given an action to take
+    def result(self, state, action):
+        new_state = list(state)
+        on_left = state[2]
+        pos_or_neg = 1
+
+
+        if on_left : pos_or_neg = -1
+
+        """If we encounter an M in action, we subtract 1 from the current amount of
+            M if the boat is to go right, and we add 1 if the boat is to go
+            left. We do the same for Cs.
+        """
+        for char in action:
+            if char == 'M': new_state[0] += pos_or_neg
+            if char == 'C': new_state[1] += pos_or_neg
+
+        #set the new location of the boat
+        new_state[2] = not new_state[2]
+
+        return tuple(new_state)
+
     
     #returns True if current state is equal to the goal state
     def goal_test(self, state):
@@ -71,6 +94,15 @@ class MissCannibals(Problem):
 
 if __name__ == '__main__':
     initial_state = (3, 3, True)
+    new_state = initial_state
     misscann = MissCannibals(initial_state)
-    print(misscann.actions(initial_state))
+    path = depth_first_graph_search(misscann).solution()
+
+    print(path)
+    print(initial_state)
+    for item in path:
+        new_state = misscann.result(new_state, item)
+        print(new_state)
+
+
 
